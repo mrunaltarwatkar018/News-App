32be4659759a8287b6fd01b92ef3ef13add481b4
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-06-23 23:34:17 +0530

    Initial Commit

---
+++ src/components/News.js
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+
+export class News extends Component {
+    articles = [
+        {
+        source: {
+            id: "bbc-sport",
+            name: "BBC Sport",
+        },
+        author: null,
+        title:
+            "England vs South Africa LIVE: ICC T20 World Cup 2024 - Super 8s cricket score, commentary, video highlights & updates",
+        description:
+            "England play South Africa in the Men's T20 World Cup Super 8s - follow text updates, video highlights and radio commentary.",
+        url: "http://www.bbc.co.uk/sport/cricket/live/c2qqz1g5wy0t",
+        urlToImage:
+            "https://static.files.bbci.co.uk/ws/simorgh-assets/public/sport/images/metadata/poster-1024x576.png",
+        publishedAt: "2024-06-21T14:07:16.869133Z",
+        content:
+            "England captain Jos Buttler: \"It's a fresh wicket, looks good, so we fancy another chase.\r\n\"The start time could potentially make a difference in terms of the moisture, but it's a good pitch and we b… [+545 chars]",
+        },
+        {
+        source: {
+            id: "news-com-au",
+            name: "News.com.au",
+        },
+        author: "Andrew McMurtry",
+        title: "Ex-Test star dies suddenly aged 52",
+        description:
+            "The Indian cricket community has been left in shock after the death of former Test cricketer David Johnson at the age of 52.",
+        url: "https://www.news.com.au/sport/cricket/deeply-saddened-former-indian-test-player-david-johnson-dead-at-52/news-story/2f5a8340a062857c8cabca6c0f2d91ba",
+        urlToImage:
+            "https://content.api.news/v3/images/bin/e43934f8c809a22c530227c7dce2a141",
+        publishedAt: "2024-06-21T09:38:00Z",
+        content:
+            "The Indian cricket community has been left in shock after the death of former Test cricketer David Johnson at the age of 52.\r\nJohnson had been battling health issues and alcohol addiction over the pa… [+3287 chars]",
+        },
+        {
+        source: {
+            id: "espn-cric-info",
+            name: "ESPN Cric Info",
+        },
+        author: null,
+        title:
+            "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+        description:
+            "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+        url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+        urlToImage:
+            "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+        publishedAt: "2020-04-27T11:41:47Z",
+        content:
+            "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
+        },
+        {
+        source: {
+            id: "espn-cric-info",
+            name: "ESPN Cric Info",
+        },
+        author: null,
+        title:
+            "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+        description:
+            "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+        url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+        urlToImage:
+            "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+        publishedAt: "2020-03-30T15:26:05Z",
+        content:
+            "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
+        },
+    ];
+
+    constructor() {
+        super();
+        console.log("Hello, I am constructor from News Component");
+
+        this.state = {
+        articles: this.articles,
+        loading: false,
+        };
+    }
+
+    render() {
+        return (
+        <div className="container my-4">
+            <h2> News App - Top Headlines </h2>
+
+            <div className="row">
+            <div className="col-md-4">
+                <NewsItem
+                title="myTitle"
+                description="mydesc"
+                imageUrl="https://static.files.bbci.co.uk/ws/simorgh-assets/public/sport/images/metadata/poster-1024x576.png"
+                newsUrl="TODO"
+                />
+            </div>
+
+            <div className="col-md-4">
+                <NewsItem title="myTitle" description="mydesc" />
+            </div>
+
+            <div className="col-md-4">
+                <NewsItem title="myTitle" description="mydesc" />
+            </div>
+            </div>
+        </div>
+        );
+    }
+}
+
+export default News;
