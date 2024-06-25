387ff93c7c683a1068f9495727a9062611efabfc
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-06-26 01:55:20 +0530

    Hiding API Key by Adding Custom Environment Variables

--- src/components/News.js
+++ src/components/News.js
@@ -64,7 +64,7 @@ export class News extends Component {
 
     fetchMoreData = async () => {
         this.setState({page: this.state.page + 1});
-        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=1edd0795320c4cedbb95d9f01a286897&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({
